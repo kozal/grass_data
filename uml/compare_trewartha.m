282057d556361412852d1df50201c889fbd5fe1f
aok <omer.kozal@metu.edu.tr>
2017-07-23 18:25:43 +0300

    trewartha colors added

--- uml/compare_trewartha.m
+++ uml/compare_trewartha.m
@@ -58,7 +58,7 @@ R_old = [-0.5 0
 % referencing object
 R = georasterref('RasterSize', [361 576], 'ColumnsStartFrom', 'north', 'Latlim', [-90 90], ...
 		'Lonlim', [-180 180]);
-	
+
 figure(1)
 clf
 geoshow(tr84_88, cmap_trew, R);
@@ -77,3 +77,33 @@ colormap(cmap_trew)
 h=colorbar;
 set(h,'YTickMode','manual','YTick',(256/length(cmap_trew))*[1:length(cmap_trew)],'YTickLabelMode','manual','YTickLabel',labels);
 
+%% trewartha colors
+load ../mat/trewartha_84_88.mat
+cmap_trew = [[255   255   255];     %  0 masked out
+             [128     0     0];     %  1 Ar, tropical rainy
+             [192     0     0];     %  2 Am, Tropical monsoonal
+             [204     0     0];     %  3 Aw, Tropical wet and dry
+             [179   107     0];     %  4 BSh, Tropical-subtopical steppe (hot) 
+             [213   128     0];     %  5 BSk, Temperate-boreal steppe (cold)
+             [242   146     0];     %  6 BWh, Tropical-subtopical desert (hot) 
+             [255   183    74];     %  7 BWk, Temperate-boreal desert (cold)
+             [100   132    64];     %  8 Cwa, Subtropical humid (hot)
+             [131   159    79];     %  9 Cwb, Subtropical humid (warm)
+             [162   186   116];     % 10 Cfa, Subtropical dry winter (hot)
+             [201   201   148];     % 11 Cfb, Subtropical dry winter (warm)
+             [  0    32    64];     % 12 DO, Temperate ocianic % D series colormap is different than Belda
+             [  0    64   128];     % 13 DCsa, Temperate dry summer (hot)
+             [  0   100   200];     % 14 DCsb, Temperate dry summer (warm)
+             [  0   128   255];     % 15 DCwa, Temperate dry winter (hot)
+             [ 72   164   255];     % 16 DCwb, Temperate dry winter (warm)
+             [128     0   128];     % 17 DCfa, Temperate humid (hot)
+             [208     0   208];     % 18 DCfb, Temperate humid (warm)
+             [253   218    19];     % 19 E, Boreal
+             [185   185   255];     % 20 Ft, Tundra-Alpine
+             [228   247   254];     % 21 Fi, Glacier-Icecap
+             [  0     0     0];     % 22
+            ]./255 ;
+
+image(tr84_88);
+colormap(cmap_trew);
+impixelinfo
